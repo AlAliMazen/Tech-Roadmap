a8af6670a0d0ba31879a0b0c6eecea5aeca5c376
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-08-30 19:43:31 +0200

    Code: Interceptors to refresh the access tokens and keep user logged in

--- src/api/axiosDefault.js
+++ src/api/axiosDefault.js
@@ -3,3 +3,8 @@ import axios from "axios"
 axios.defaults.baseURL = "https://tech-roadmap-drf-6a7361986bbb.herokuapp.com/"
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+// create the axios interceptor for request and response
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
--- src/context/CurrentUserContext.js
+++ src/context/CurrentUserContext.js
@@ -1,6 +1,9 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
 
 import axios from "axios";
+import { axiosRes } from "../api/axiosDefault";
+import { axiosReq } from "../api/axiosDefault";
+import { response } from "msw";
 
 //the following two varibale, has to do with context variable
 export const CurrentUserContext = createContext();
@@ -12,10 +15,12 @@ export const useSetCurrentUser = () => useContext(SetCurrentUserContext)
 export const CurrentUserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   // to get who and whether the user is looged in, we need to send
+    const history = useHistory()
+
   // Network request
   const handleMount = async () => {
     try {
-      const { data } = await axios.get("dj-rest-auth/user/");
+      const { data } = await axiosRes.get("dj-rest-auth/user/");
       setCurrentUser(data);
     } catch (err) {
       console.log(err);
@@ -24,6 +29,49 @@ export const CurrentUserProvider = ({ children }) => {
   useEffect(() => {
     handleMount();
   }, []);
+
+
+  useMemo(()=>{
+        axiosReq.interceptors.request.use(
+            async (config) => {
+                try{
+                    await axios.post("/dj-rest-auth/tokem/refresh/")
+                }catch(err){
+                    setCurrentUser((prevCurrentUser) => {
+                        if (prevCurrentUser) {
+                            history.push("/signin")
+                        }
+                        return null;
+                    });
+                    return config;
+                }
+                return config;
+            }, (err) => {
+                return Promise.reject(err);
+            }
+        );
+        // axiosRes for keep the user logged in for 24 hrs
+        axiosRes.interceptors.response.use(
+            (response) => response,
+            async(err) => {
+                if (err.response?.status === 401 ){
+                    try{
+                        await axios.post('/dj-rest-auth/refresh/')
+                    }catch(err){
+                        setCurrentUser(prevCurrentUser => {
+                            if (prevCurrentUser){
+                                history.push('/signin')
+                            }
+                            return null
+                        })
+                    }
+                    return axios(err.config)
+                }
+                return Promise.reject(err)
+            }
+        )
+  }, [history])
+
   return (
     <CurrentUserContext.Provider value={currentUser}>
       <SetCurrentUserContext.Provider value={setCurrentUser}>
