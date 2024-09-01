07e3581fd799b939db56f8458255e73177f5f3ca
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-09-01 03:24:36 +0000

    Code: create the view article page with its styles

--- src/pages/posts/CategoryPage.js
+++ src/pages/posts/CategoryPage.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefault";
 
 function CategoryPage() {
   // Add your logic here
+  const { id } = useParams();
+  const [category, setCategory] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: category }] = await Promise.all([
+          axiosReq.get(`/categories/${id}`),
+        ]);
+        setCategory({ results: [category] });
+        console.log(category);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [id]);
 
 
   return (
@@ -16,7 +36,7 @@ function CategoryPage() {
         <p>Category Title</p>
         <p>Category content</p>
         <Container className={appStyles.Content}>
-          Comments
+          {category.title}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
