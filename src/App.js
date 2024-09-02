98e40ec9b592b22c627280cc66a99f96d413368e
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-09-02 09:48:56 +0000

    Code: add the PostsPage with its home link and its CSS

--- src/App.js
+++ src/App.js
@@ -6,32 +6,62 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostcreateForm";
 import PostPage from "./pages/posts/PostPage";
-import CategoryPage from "./pages/posts/CategoryPage";
-import CoursesPage from "./pages/posts/CoursesPage";
+import CategoryPage from "./pages/categories/CategoryPage";
+import CoursesPage from "./pages/courses/CoursesPage";
+import PostsPage from "./pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 
 
 
 function App() {
- 
+  //we nee to know the current user 
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
-   
-        <div className={styles.App}>
-          <NavBar />
-          <Container className={styles.Main}>
-            <Switch>
-              <Route exact path="/" render={() => <h1>Home Page</h1>} />
-              <Route exact path="/courses" render={() => <CoursesPage/>} />
-              <Route exact path="/articles/:id" render={() => <PostPage />  } />
-              <Route exact path="/categories" render={() => <CategoryPage/>}/>
-              <Route exact path="/signin" render={() => <SignInForm />} />
-              <Route exact path="/signup" render={() => <SignUpForm />} />
-              <Route exact path="/articles/create" render = {() => <PostCreateForm/>}/>
-              <Route render={() => <p>PAGE NOT FOUND</p>} />
-            </Switch>
-          </Container>
-        </div>
-      
+
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route 
+            exact path="/" 
+            render={() => (
+            <PostsPage message="No results found. Adjust the search keyword." />
+            )} 
+          />
+          <Route 
+            exact path="/Feed" 
+            render={() => (
+            <PostsPage 
+            message="No results found. Adjust the search keyword or follow a user."
+            filter={`owner__followed__owner__profile=${profile_id}&`}
+            />
+            )} 
+          />
+          <Route 
+            exact path="/liked" 
+            render={() => (
+            <PostsPage 
+              message="No results found. Adjust the search keyword or like a post." 
+              filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&` }
+            
+            />
+            )} 
+          />
+          <Route exact path="/" render={() => (<PostsPage message="No results found. Adjust the search keyword." />)} />
+          <Route exact path="/courses" render={() => <CoursesPage />} />
+          <Route exact path="/articles/:id" render={() => <PostPage />} />
+          <Route exact path="/categories" render={() => <CategoryPage />} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/articles/create" render={() => <PostCreateForm />} />
+          <Route render={() => <p>PAGE NOT FOUND</p>} />
+        </Switch>
+      </Container>
+    </div>
+
   );
 }
 
--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -4,6 +4,8 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
+import Accordion from 'react-bootstrap/Accordion'
+import { axiosRes } from "../../api/axiosDefault";
 
 const Post = (props) => {
   const {
@@ -19,12 +21,47 @@ const Post = (props) => {
     image,
     updated_at,
     postPage,
+    setPosts,
   } = props;
 
   //to get the current logged in user
   const currentUser = useCurrentUser();
   //check if the current user is the same as the owner 
   const is_owner = currentUser?.username === owner;
+
+  //handling likes
+  const handleLike = async () => {
+    try {
+      console.log("post Id is : ", {id})
+      const { data } = await axiosRes.post("/likes/",{article:id});
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.map((post) => {
+          return post.id === id
+            ? { ...post, likes_count: post.likes_count + 1, like_id: data.id }
+            : post;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+      console.log("Error:", err.response.data);
+      console.log("Error:", err.response ? err.response.data : err.message);
+    }
+  };
+
+  //handle unlike
+  const handleUnlike = async ()=>{
+    try{
+      await axiosRes.delete(`/likes/${like_id}`);
+      setPosts((prevPosts)=>({
+        ...prevPosts,
+        results: prevPosts.results.map((post)=>{
+          return post.id === id ? {...post, likes_count:post.likes_count-1, like_id:null}: post;
+        })
+      }))
+    }catch(err){
+      console.log(err)}
+  };
 
   return (
     <Card className={styles.Post}>
@@ -46,9 +83,19 @@ const Post = (props) => {
         <Card.Img src={image} alt={title} />
       </Link>
 
+      <Accordion defaultActiveKey="1">
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="0">
+            {title && <Card.Title className="text-center">{title}</Card.Title>}
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>{content && <Card.Text>{content}</Card.Text>}</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        
+      </Accordion>
+
       <Card.Body>
-        {title && <Card.Title className="text-center">{title}</Card.Title>}
-        {content && <Card.Text>{content}</Card.Text>}
         <div className={styles.PostBar}>
           {is_owner ? (
             <OverlayTrigger
@@ -58,11 +105,11 @@ const Post = (props) => {
               <i className="far fa-heart" />
             </OverlayTrigger>
           ) : like_id ? (
-            <span onClick={() => {}}>
+            <span onClick={handleUnlike}>
               <i className={`fas fa-heart ${styles.Heart}`} />
             </span>
           ) : currentUser ? (
-            <span onClick={() => {}}>
+            <span onClick= { handleLike }>
               <i className={`far fa-heart ${styles.HeartOutline}`} />
             </span>
           ) : (
