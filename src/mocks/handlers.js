219fda0b20128db6911eed0200e11f9101019b12
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-09-10 15:53:41 +0000

    Code: Starting with Tests and creating the mocks

---
+++ src/mocks/handlers.js
@@ -0,0 +1,22 @@
+import { rest } from "msw";
+
+const baseURL = "https://tech-roadmap-drf-6a7361986bbb.herokuapp.com/";
+
+export const handlers = [
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        "pk": 2,
+        "username": "mazen",
+        "email": "",
+        "first_name": "",
+        "last_name": "",
+        "profile_id": 2,
+        "profile_image": "https://res.cloudinary.com/dupa85w3z/image/upload/v1/media/images/img_187_qp1fpr"
+        })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
